b96ff9380f466f8a8d2c2ecf235e916e6d3d0f98
Abdur Rafay Ahmed <a_rafay01@hotmail.com>
2023-10-22 20:46:48 +0500

    CODSOFT LEVEL 1 PROJECTS UPDATED

---
+++ calculator-app/script.js
@@ -0,0 +1,31 @@
+let input = document.getElementById('inputBox');
+
+let string = "";
+let buttons = document.querySelectorAll('button');
+
+// let arr = Array.from(buttons);
+// arr.forEach(button =>{
+// })
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval (string);
+            document.querySelector('input').value = string; 
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }         
+        
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;         
+        }
+    })
+})
